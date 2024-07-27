3024d823fad71dabfa6b8a82878f9f63e34ee271
sdspot2034 <shreyandas2034@gmail.com>
2024-07-27 19:45:37 +0530

    Improved Authorization workflow

--- authorization.py
+++ authorization.py
@@ -4,7 +4,7 @@ import base64
 import requests
 import json
     
-def first_authorization(client_id, redirect_url):
+def first_authorization(client_id, redirect_url, auth_file):
     code = None
     app = flask.Flask(__name__)
 
@@ -19,6 +19,7 @@ def first_authorization(client_id, redirect_url):
         ,"user-library-read"
         ,"user-read-email"
         ,"user-read-private"])
+    print("scopes defined")
 
     @app.route("/")
     def index():
@@ -27,7 +28,7 @@ def first_authorization(client_id, redirect_url):
             'response_type': 'code',
             'scope': scopes,
             'redirect_uri': redirect_url,
-        })  
+        })
         return flask.redirect(user_auth_url)
 
     @app.route("/callback")
@@ -38,7 +39,7 @@ def first_authorization(client_id, redirect_url):
             shutdown_server()
             return f"Authorization failed with the following error: {err}"
         else:
-            return code
+            with open(auth_file, 'w+') as file: file.write(code)
             shutdown_server()
             return "<h1> Authorization successful. Please close this window. </h1>"
 
@@ -47,11 +48,33 @@ def first_authorization(client_id, redirect_url):
         if func is None:
             raise RuntimeError('Not running with the Werkzeug Server')
         func()
+    
+    app.run(host="localhost", port=5050)
 
-    if __name__ == "__main__":
-        app.run(host="localhost", port=5050)
-        
-        
+    
+    
+def peek_file(file_path, read_mode='r'):
+    try:
+        file = open(file_path, read_mode)
+        return True
+    except FileNotFoundError: return False
+
+
+
+def get_auth_code(auth_file):
+    with open(auth_file, 'r') as file:
+        auth_code = file.read()
+    return auth_code
+
+    
+def authorize_app(client_id, redirect_url, auth_file = 'auth_code.txt'):
+    authorized = peek_file(auth_file)
+
+    if not authorized:
+        first_authorization(client_id, redirect_url, auth_file)
+
+    return get_auth_code(auth_file)
+
 
 def get_access_token(client_id, client_secret):
     auth_string = client_id + ":" + client_secret
