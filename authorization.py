e0985117e2afa6bf41d301c1d32f28195c359f21
sdspot2034 <shreyandas2034@gmail.com>
2024-07-31 22:25:01 +0530

    Major revamp: Object-oriented, Flask Wrapper, Refresh timer Complete overhaul of Spotify Authorisation. Changed Authorisation mechanism to a class-based approach. Installed a separate wrapper function for Flask App for authorisation. Minimised file handling to ensure sensitive content remains within program. Access tokens spawned will take into consideration refresh timer -- to be tested further.

--- authorization.py
+++ authorization.py
@@ -1,4 +1,4 @@
-import flask
+from flask import Flask, redirect, request
 import urllib.parse as urlparse
 import base64
 import requests
@@ -7,6 +7,58 @@ import os
 import time
 from functools import wraps
 
+
+# Mini web app to procure authorization
+class FlaskWrapper():
+    def __init__(self, client_id, redirect_url, scopes):
+        self.client_id = client_id
+        self.redirect_url = redirect_url
+        self.scopes = scopes
+        self.auth_code = None
+        self.app = Flask(__name__)
+        self.setup_routes()
+        
+    def setup_routes(self):
+        @self.app.route("/")
+        def index():
+            user_auth_url = 'https://accounts.spotify.com/authorize?' + urlparse.urlencode({
+                'client_id': self.client_id,
+                'response_type': 'code',
+                'scope': self.scopes,
+                'redirect_uri': self.redirect_url,
+            })
+            return redirect(user_auth_url)
+
+        @self.app.route("/callback")
+        def callback():
+            self.auth_code = request.args.get('code')
+            if self.auth_code is None:
+                err = request.args.get('error')
+                self.shutdown_server()
+                return f"Authorization failed with the following error: {err}"
+            else:
+                self.shutdown_server()
+                return "<h1> Authorization successful. Please close this window. </h1>"
+            
+            
+    def shutdown_server(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+
+    def run(self):
+        self.app.run(host="localhost", port=5050)
+
+    def get_auth_code(self):
+        return self.auth_code
+    
+    def __del__():
+        # Ensure auth_code is removed from flask app object to prevent misuse
+        self.auth_code = None
+        print("Flask authorization micro-app destructed.")
+
+
 class SpotifyAuth:
     def __init__(self, client_id, client_secret, redirect_url):
         self.client_id = client_id
@@ -14,11 +66,7 @@ class SpotifyAuth:
         self.redirect_url = redirect_url
         self.auth_code = None
         self.token = None
-
-    def first_authorization(self):
-        code = None
-        app = flask.Flask(__name__)
-        scopes = " ".join([
+        self.scopes = " ".join([
             "user-read-playback-state",
             "user-read-currently-playing",
             "playlist-read-private",
@@ -30,37 +78,13 @@ class SpotifyAuth:
             "user-read-email",
             "user-read-private"
         ])
-        print("scopes defined")
 
-        @app.route("/")
-        def index():
-            user_auth_url = 'https://accounts.spotify.com/authorize?' + urlparse.urlencode({
-                'client_id': self.client_id,
-                'response_type': 'code',
-                'scope': scopes,
-                'redirect_uri': self.redirect_url,
-            })
-            return flask.redirect(user_auth_url)
-
-        @app.route("/callback")
-        def callback():
-            code = flask.request.args.get('code')
-            if code is None:
-                err = flask.request.args.get('error')
-                self.shutdown_server()
-                return f"Authorization failed with the following error: {err}"
-            else:
-                code = self.auth_code
-                self.shutdown_server()
-                return "<h1> Authorization successful. Please close this window. </h1>"
-
-        def shutdown_server():
-            func = flask.request.environ.get('werkzeug.server.shutdown')
-            if func is None:
-                raise RuntimeError('Not running with the Werkzeug Server')
-            func()
-
-        app.run(host="localhost", port=5050)
+    def first_authorization(self):
+        flask_app = FlaskWrapper(self.client_id, self.redirect_url, self.scopes)
+        flask_app.run()
+        self.auth_code = flask_app.get_auth_code()
+        del flask_app         # Destroy app object
+        
 
     def get_auth_code(self):
         return self.auth_code
@@ -76,18 +100,17 @@ class SpotifyAuth:
         else:
             self.set_auth_code_from_file(auth_file)
 
-
     def set_access_tokens(self, auth_file = None):
         auth_string = self.client_id + ":" + self.client_secret
         auth_bytes = auth_string.encode("utf-8")
         auth_encoded = str(base64.b64encode(auth_bytes), "utf-8")
 
-        auth_code = self.authorize_app(auth_file)
+        self.authorize_app(auth_file)
 
         token_endpoint = "https://accounts.spotify.com/api/token"
         data = {
             "grant_type": "authorization_code",
-            "code": auth_code,
+            "code": self.auth_code,
             "redirect_uri": self.redirect_url,
         }
         headers = {
@@ -115,20 +138,21 @@ class SpotifyAuth:
         self.token = token_details
         
     
-    def get_access_token(self, token_file = None, auth_file = None):
+    def get_access_token(self, token_file = None, auth_file = None, update_file=False):
         if not self.token and not token_file:
             self.set_access_token(auth_file)
         elif not self.token:
             self.set_access_token_from_file(token_file)
-            
-        access_token_exists = os.path.isfile(token_file)
 
-        if int(time.time()) >= token_details['expires_at']:
-            token_details = self.refresh_access_tokens(token_details['refresh_token'])
-            with open(self.token_file, 'w+') as file:
-                json.dump(token_details, file, indent=4)
+        if int(time.time()) >= self.token['expires_at']:
+            self.token = self.refresh_access_tokens(self.token['refresh_token'])
+            if update_file and not token_file:
+                print("Token file location not provided. Proceeding without updating file, please use method save_access_token_to_file for updating file.")
+            elif update_file:
+                with open(token_file, 'w+') as file:
+                    json.dump(self.token, file, indent=4)
 
-        return token_details['access_token']
+        return self.token['access_token']
 
     def refresh_access_tokens(self, refresh_token):
         auth_string = self.client_id + ":" + self.client_secret
@@ -157,3 +181,11 @@ class SpotifyAuth:
             access_token = self.get_access_token()
             return func(access_token, *args, **kwargs)
         return wrapper
+
+    def save_auth_code_to_file(self, auth_file):
+        with open(auth_file, 'w+') as file:
+            file.write(json.dumps(self.auth_code, indent=4))
+            
+    def save_access_token_to_file(self, token_file):
+        with open(token_file, 'w+') as file:
+            file.write(json.dumps(self.token, indent=4))
