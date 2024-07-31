641265e906f2b4a65b34b5a7acf2a69933749ba6
sdspot2034 <shreyandas2034@gmail.com>
2024-07-31 23:36:51 +0530

    Used result.json() & Added play history

--- spotify_functions.py
+++ spotify_functions.py
@@ -2,7 +2,8 @@ import requests
 import json
 
 ENDPOINTS = {
-    "user-details":"https://api.spotify.com/v1/me"
+    "user-details":"https://api.spotify.com/v1/me",
+    "play-history":"https://api.spotify.com/v1/me/player/recently-played"
 }
 
 
@@ -12,4 +13,15 @@ def get_user_details(access_token):
         "Content-type": "application/x-www-form-urlencoded"
     }
     result = requests.get(ENDPOINTS['user-details'], headers=headers)
-    return json.loads(result.content)
+    return result.json()
+
+def get_listening_history(access_token, cdc_time, limit=50):
+    headers={"Authorization": "Bearer " + access_token}
+    params={"after": str(cdc_time), "limit":str(limit)}
+    result = requests.get(ENDPOINTS['play-history'], headers=headers, params=params)
+    track_list = result.json()
+    for item in track_list['items']:
+        if 'available_markets' in item['track']: del item['track']['available_markets']
+        if 'available_markets' in item['track']['album']: del item['track']['album']['available_markets']
+    return track_list
+    
