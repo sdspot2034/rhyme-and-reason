d6cade725a4d35410ae241645401cb0bb951d9d4
sdspot2034 <shreyandas2034@gmail.com>
2024-08-04 17:42:04 +0530

    Added module for SQL functions.

---
+++ sql_functions.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import insert
+
+
+with open('SQL_CREDENTIALS.json', 'r') as file:
+    cred = json.loads(file.read())
+    
+user = cred['user']
+pswd = cred['pass']
+host = cred['host']
+target_engine = create_engine(f'mysql+mysqlconnector://{user}:{pswd}@{host}:3306/spotify_db')
+
+
+def save_to_sql(df, table, key_cols, wid, other_cols, mode):
+    
+    list_of_records = df.to_dict(orient='records')
+    all_columns = table.columns
+    
+    if 'mode' == 'upsert':
+        insert_stmt = insert(table).values(list_of_records)
+        update_columns = {c_name: insert_stmt.inserted[c_name] for c_name in all_columns if c_name not in key_cols}
+        upsert_stmt = insert_stmt.on_duplicate_key_update(update_columns)
+        
+        with target_engine.connect() as connection:
+            connection.execute(upsert_stmt)
+
+    
