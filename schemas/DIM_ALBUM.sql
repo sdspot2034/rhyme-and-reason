579a746ecb5b111e136b6760c42d2e64a3761149
sdspot2034 <shreyandas2034@gmail.com>
2024-08-05 21:33:40 +0530

    Created Dimension for Album.

---
+++ schemas/DIM_ALBUM.sql
@@ -0,0 +1,10 @@
+CREATE TABLE DIM_ALBUM (
+    album_wid INT AUTO_INCREMENT PRIMARY KEY
+    , album_id VARCHAR(62) NOT NULL UNIQUE
+    , album_type VARCHAR(20) NOT NULL 
+    , album_name TEXT NOT NULL
+    , release_date DATE
+    , release_date_precision VARCHAR(20) 
+    , total_tracks INT
+    , cover_image_url TEXT
+)
