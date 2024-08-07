b6a1bb60fab30d67136ca9c6e4abc5c80032a7af
sdspot2034 <shreyandas2034@gmail.com>
2024-08-07 22:16:01 +0530

    Fixed INSERT IGNORE bug.

--- schemas/BRIDGE_ALBUM_ARTIST.sql
+++ schemas/BRIDGE_ALBUM_ARTIST.sql
@@ -1,5 +1,6 @@
 CREATE TABLE BRIDGE_ALBUM_ARTIST (
-    album_artist_map_wid  INT AUTO_INCREMENT PRIMARY KEY
-    , album_wid INT
-    , artist_wid INT
+    album_artist_map_wid INT AUTO_INCREMENT PRIMARY KEY
+    , album_wid INT DEFAULT NULL
+    , artist_wid INT DEFAULT NULL
+    , UNIQUE KEY album_artist_map (album_wid, artist_wid)
 )
