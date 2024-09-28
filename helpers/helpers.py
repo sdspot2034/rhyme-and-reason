1ddee59462567d27e2e5595a85ee09391009c078
sdspot2034 <shreyandas2034@gmail.com>
2024-09-28 14:08:02 +0530

    Added helper function to_datetime to convert numpy datetime to raw python datetime.

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -2,3 +2,19 @@ def get_url(x,height=640):
     for elem in x:
         if elem['height'] == height: return elem['url']
     return None
+
+
+def to_datetime(date):
+    """
+    Converts a numpy datetime64 object to a python datetime object 
+    Input:
+      date - a np.datetime64 object
+    Output:
+      DATE - a python datetime object
+    """
+    import numpy as np
+    from datetime import datetime
+
+    timestamp = ((date - np.datetime64('1970-01-01T00:00:00'))
+                 / np.timedelta64(1, 's'))
+    return datetime.utcfromtimestamp(timestamp)
